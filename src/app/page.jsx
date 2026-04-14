33198e8ef6fa17196e1aaf020d2c645d11b76449
Kawsar37 <kawsarali750@gmail.com>
2026-04-14 08:27:13 +0600

    project setup done with next js

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-red-500 text-white">Hello World</h1>
+      <button className="btn btn-primary">Save</button>
+    </div>
+  );
+}
